d528d1b794f87f8020a4101d825b627f2270d1d4
lenardsk <lenardskarklins64@gmail.com>
2022-09-30 11:33:11 +0300

    Class_04

---
+++ Class_06/first.c
@@ -0,0 +1,20 @@
+// komentāra viena rinda - //tiek apzimēts tikai viens komentāra sākums
+//komentāra cita rinda
+
+/*
+Komentāra paragrāfs
+izmanto, piemēram, lai komentētu koda fragmentus
+*/
+
+int main()
+ {
+ //if()
+  //{
+  //
+  //}
+
+ return 0;
+
+ // return 100;
+ }
+
